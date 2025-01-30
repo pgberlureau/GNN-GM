6b8b1e0d09a66979130bca552cf5c8a20ba6e5ef
Gabriel Desfrene <gabriel@desfrene.fr>
2025-01-30 19:42:07 +0100

    Preparing for Collab

--- train_gnn.py
+++ train_gnn.py
@@ -1,24 +1,62 @@
 import argparse
+from datetime import datetime
 from pathlib import Path
 
+import numpy as np
 from torch_geometric import nn
 from torch_geometric.loader import DataLoader
 import torch
 from tqdm import tqdm
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
-from dataset import GraphChessDataset, build_train_test_dataset
+from dataset import build_train_test_dataset
+from matplotlib import pyplot as plt
 
 
-# 10 epoch | 100_000 20min
-#
-#
-# 90/10 Train/Test
-#  1 Job avec 1_000_000
+def plot_metrics(out_dir, num_epochs, train_loss_list, train_accuracy_list, train_precision_list, train_recall_list,
+                 test_loss_list, test_accuracy_list, test_precision_list, test_recall_list):
+    # Training Metrics
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.semilogy(range(num_epochs), train_loss_list, label='loss')
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Loss")
+    ax.set_title("Training Loss")
+    ax.legend()
+    fig.savefig(out_dir / f"train_loss.png")
+    plt.close(fig)
 
-# RAM 20Go - 32Go
-# Disk: 20Go
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(range(num_epochs), train_precision_list, label='precision')
+    ax.plot(range(num_epochs), train_recall_list, label='recall')
+    ax.plot(range(num_epochs), train_accuracy_list, label='accuracy')
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Metrics")
+    ax.set_title("Training Metrics (Accuracy, Precision, Recall)")
+    ax.legend()
+    fig.savefig(out_dir / f"train.png")
+    plt.close(fig)
 
-# Conda
+    # Test Metrics
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.semilogy(range(num_epochs), test_loss_list, label='test loss')
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Loss")
+    ax.set_title("Test Loss")
+    ax.legend()
+    fig.savefig(out_dir / f"test_loss.png")
+    plt.close(fig)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(range(num_epochs), test_precision_list, label='test precision')
+    ax.plot(range(num_epochs), test_recall_list, label='test recall')
+    ax.plot(range(num_epochs), test_accuracy_list, label='test accuracy')
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Metrics")
+    ax.set_title("Test Metrics (Accuracy, Precision, Recall)")
+    ax.legend()
+    fig.savefig(out_dir / f"test.png")
+    plt.close(fig)
+
 
 class Model(torch.nn.Module):
     def __init__(self, heads_nb=1, num_nodes=64, in_channels=15, hidden_channels=64, out_channels=10):
@@ -56,12 +94,39 @@ class Model(torch.nn.Module):
 
 def centipawn_transform(cp: torch.int16) -> torch.Tensor:
     res = torch.zeros(2, dtype=torch.float)
-    res[cp >= 0] = 1
+    res[int(cp >= 0)] = 1
     return res
 
 
-def train(root: str, dataset_size: int, num_epochs: int, batch_size: int, load_workers: int, model_save: str):
-    model_save = Path(model_save)
+def test(model, criterion, test_dataloader, device):
+    total_test_loss = 0.0
+    test_preds = []
+    test_labels = []
+
+    with torch.no_grad():
+        for data in test_dataloader:
+            data = data.to(device)
+            output = model(data)
+            loss = criterion(output, data.y)
+            total_test_loss += loss.item()
+
+            preds = torch.argmax(output, dim=1).cpu().numpy()
+            labels = torch.argmax(data.y, dim=1).cpu().numpy()
+
+            test_preds.extend(preds)
+            test_labels.extend(labels)
+
+    # Compute test metrics
+    test_loss = total_test_loss / len(test_dataloader)
+    test_accuracy = accuracy_score(test_labels, test_preds)
+    test_precision = precision_score(test_labels, test_preds, average='weighted', zero_division=0)
+    test_recall = recall_score(test_labels, test_preds, average='weighted', zero_division=0)
+
+    return test_loss, test_accuracy, test_precision, test_recall
+
+
+def train(root: str, dataset_size: int, num_epochs: int, batch_size: int, load_workers: int, out_dir: str):
+    out_dir = Path(out_dir)
 
     # concat_x controls how the x field is concatenated by TorchGeometric.
     # Use False to create a new axis for the concatenation.
@@ -90,21 +155,77 @@ def train(root: str, dataset_size: int, num_epochs: int, batch_size: int, load_w
     model.to(device)
     model.train()
 
+    train_loss_list = []
+    train_accuracy_list = []
+    train_precision_list = []
+    train_recall_list = []
+
+    test_loss_list = []
+    test_accuracy_list = []
+    test_precision_list = []
+    test_recall_list = []
+
     for epoch in range(num_epochs):
         print(f"\nEpoch {epoch + 1}/{num_epochs}")
+
+        total_loss = 0.0
+        all_preds = []
+        all_labels = []
 
         for data in tqdm(train_loader, total=nb_batch):
             data = data.to(device)
             optimizer.zero_grad()
 
+            # Forward pass
             output = model(data)
 
+            # Compute loss
             loss = criterion(output, data.y)
-            loss.backward()
+            total_loss += loss.item()
 
+            # Backward pass and optimization
+            loss.backward()
             optimizer.step()
 
-    torch.save(model.state_dict(), model_save)
+            # Collect predictions and labels
+            preds = torch.argmax(output, dim=1).cpu().numpy()
+            labels = torch.argmax(data.y, dim=1).cpu().numpy()
+
+            all_preds.extend(preds)
+            all_labels.extend(labels)
+
+        # Compute metrics
+        epoch_loss = total_loss / len(train_loader)
+        epoch_accuracy = accuracy_score(all_labels, all_preds)
+        epoch_precision = precision_score(all_labels, all_preds, average='weighted', zero_division=0)
+        epoch_recall = recall_score(all_labels, all_preds, average='weighted', zero_division=0)
+
+        test_loss, test_accuracy, test_precision, test_recall = test(model, criterion, test_loader, device)
+
+        train_loss_list.append(epoch_loss)
+        train_accuracy_list.append(epoch_accuracy)
+        train_precision_list.append(epoch_precision)
+        train_recall_list.append(epoch_recall)
+
+        test_loss_list.append(test_loss)
+        test_accuracy_list.append(test_accuracy)
+        test_precision_list.append(test_precision)
+        test_recall_list.append(test_recall)
+
+        # Print epoch  statistics
+        print(f"Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}, "
+              f"Precision: {epoch_precision:.4f}, Recall: {epoch_recall:.4f}")
+        print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}, "
+              f"Test Precision: {test_precision:.4f}, Test Recall: {test_recall:.4f}")
+
+    torch.save(model.state_dict(), out_dir / "model.pth")
+
+    plot_metrics(out_dir, num_epochs, train_loss_list, train_accuracy_list, train_precision_list, train_recall_list,
+                 test_loss_list, test_accuracy_list, test_precision_list, test_recall_list)
+
+    np.savez(out_dir / "stats", train_loss=train_loss_list, train_accuracy=train_accuracy_list,
+             train_precision=train_precision_list, train_recall=train_recall_list, test_loss=test_loss_list,
+             test_accuracy=test_accuracy_list, test_precision=test_precision_list, test_recall=test_recall_list)
 
 
 if __name__ == '__main__':
@@ -115,13 +236,16 @@ if __name__ == '__main__':
     parser.add_argument("--batch_size", type=int, required=True, help="Batch size for training.")
     parser.add_argument("--load_workers", type=int, required=True, help="Number of data loading workers.")
     parser.add_argument("--model_save", type=Path, required=True, help="Path to save the trained model.")
-    parser.add_argument("root", type=Path,  help="Root directory of the dataset.")
+    parser.add_argument("root", type=Path, help="Root directory of the dataset.")
 
     args = parser.parse_args()
 
     save_path = args.model_save
+    save_path.mkdir(parents=True, exist_ok=True)
+    (save_path / "keep_me").touch(exist_ok=True)
 
-    if not save_path.exists():
-        save_path.touch()
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    out_path = save_path / timestamp
+    out_path.mkdir(exist_ok=False)
 
-    train(args.root, args.dataset_size, args.num_epochs, args.batch_size, args.load_workers, args.model_save)
+    train(args.root, args.dataset_size, args.num_epochs, args.batch_size, args.load_workers, out_path)
